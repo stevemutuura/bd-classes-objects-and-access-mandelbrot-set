adf4e10e13ea71421e46c1d5cef548a30dda569d
jcassidy91 <jamie@Lambda-School-Jamie-Smallwoods-MacBook-Pro.local>
2021-07-22 11:41:26 -0700

    initial commit

---
+++ src/processing/sketches/Main.java
@@ -0,0 +1,17 @@
+package processing.sketches;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+        background(0);
+    }
+
+    public static void main(String... args) {
+        PApplet.main("processing.sketches.Main");
+    }
+}
